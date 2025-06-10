b58adbc1dbe2f06c287a17e1f9c86436a7cf879e
Gustavo Martinez <gustavo@grupobarco.mx>
2025-06-09 23:51:00 -0600

    clase 12 actualizacion de estado de un objeto  con update

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { signal } from '@angular/core';
+import { Task } from './../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,34 @@ import { signal } from '@angular/core';
 })
 export class Home {
   welcome = 'Hello, todoapp';
-  tasks = signal(['Task 1', 'Task 2', 'Task 3']);
-}
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Task 1',
+      completed: false 
+    },
+    {
+      id: Date.now(),
+      title: 'Task 2',
+      completed: false 
+    },
+    {
+      id: Date.now(),
+      title: 'Task 3',
+      completed: false  
+     }
+    ]);
+
+    changeHandler(event: Event) {
+      const input = event.target as HTMLInputElement;
+      const newTask = {
+        id: Date.now(),
+        title: input.value,
+        completed: false
+      }
+      console.log(newTask);
+      this.tasks.update((tasks) => [...this.tasks(), newTask]);
+      input.value = '';
+    }
+  }
+
